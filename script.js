0036425b7300910ce9d57779ffbb99acfafcdbd8
Tushar Goyal <tgoyal@tavisca.in>
2019-08-22 14:09:56 +0530

    added edit fucntionality

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var tags = [];
+var tags = ["Hit the gym","Pay bills", "Meet Chandan", "Buy eggs", "Read a book", "Organize office"];
 //(tags = localStorage.getItem("originalArray"))();
 
 function newElement(){
@@ -7,6 +7,7 @@ function newElement(){
   var li = document.createElement("li");
   var t = document.createTextNode(inputValue);
   li.appendChild(t);
+  addDecoration(li);
 
 
   if (inputValue === '') {
@@ -18,6 +19,7 @@ function newElement(){
 
   }
   document.getElementById("myInput").value = "";
+  // addEdit();
 }
 
  function suggest(value) {
@@ -33,17 +35,63 @@ function newElement(){
    }
  }
 
+/*even trigging when enter is pressed inside input field*/
+var input = document.getElementById("myInput");
+input.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+   event.preventDefault();
+   document.getElementById("inputButton").click();
+  }
+});
 
-
- //adding a close button in each list item
-var parent = document.getElementById('main');
-var liList = parent.getElementsByTagName('LI');
+ //adding a close and edit button in each list item
+var parent = document.getElementById('todo-div');
+var liList = parent.getElementsByTagName('li');
 
 for (var i = 0; i < liList.length; i++) {
+  addDecoration(liList[i]);
+  }
+
+function addDecoration(li){
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
   span.className = "close";
   span.appendChild(txt);
-  liList[i].appendChild(span);
-  //document.getElementById("main").appendChild(span);
+  li.appendChild(span);
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("edit");
+  span.className = "edit";
+  span.appendChild(txt);
+  li.appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+for (var i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+  var div = this.parentElement;
+  div.style.display = "none";
+  }
+}
+
+// Add a "checked" symbol when clicking on a list item
+var list = document.getElementById("myUL");
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+
+
+/*editing the field*/
+var items = document.getElementsByClassName("edit");
+for(var i=0;i<items.length;i++){
+  items[i].onclick = function(){
+    var string = this.parentElement.textContent;
+    string = string.substring(0, string.length-5);
+    document.getElementById("myInput").value = string;
+    this.parentElement.style.display = "none";
+  }
 }
